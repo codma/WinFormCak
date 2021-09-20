feccca7c224b10b0e4fcd3cc17ca303338a6b6bd
hawoond <hawoond@gmail.com>
2021-09-20 23:47:15 +0900

    초기 커밋.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace WinFormCak
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TbResult = new System.Windows.Forms.TextBox();
+            this.btPercentage = new System.Windows.Forms.Button();
+            this.btRemoveAll = new System.Windows.Forms.Button();
+            this.btCancle = new System.Windows.Forms.Button();
+            this.btBackSpace = new System.Windows.Forms.Button();
+            this.btDivision = new System.Windows.Forms.Button();
+            this.btMultiply = new System.Windows.Forms.Button();
+            this.btMinus = new System.Windows.Forms.Button();
+            this.btPlus = new System.Windows.Forms.Button();
+            this.btFloat = new System.Windows.Forms.Button();
+            this.btThree = new System.Windows.Forms.Button();
+            this.btTwo = new System.Windows.Forms.Button();
+            this.btOne = new System.Windows.Forms.Button();
+            this.btZero = new System.Windows.Forms.Button();
+            this.btSix = new System.Windows.Forms.Button();
+            this.btFive = new System.Windows.Forms.Button();
+            this.btFour = new System.Windows.Forms.Button();
+            this.btNegative = new System.Windows.Forms.Button();
+            this.btNine = new System.Windows.Forms.Button();
+            this.btEight = new System.Windows.Forms.Button();
+            this.btSeven = new System.Windows.Forms.Button();
+            this.btResult = new System.Windows.Forms.Button();
+            this.pbCuteCat = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCuteCat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TbResult
+            // 
+            this.TbResult.Font = new System.Drawing.Font("맑은 고딕", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TbResult.Location = new System.Drawing.Point(0, -1);
+            this.TbResult.Name = "TbResult";
+            this.TbResult.Size = new System.Drawing.Size(302, 41);
+            this.TbResult.TabIndex = 0;
+            this.TbResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btPercentage
+            // 
+            this.btPercentage.Location = new System.Drawing.Point(0, 46);
+            this.btPercentage.Name = "btPercentage";
+            this.btPercentage.Size = new System.Drawing.Size(71, 50);
+            this.btPercentage.TabIndex = 1;
+            this.btPercentage.Text = "%";
+            this.btPercentage.UseVisualStyleBackColor = true;
+            // 
+            // btRemoveAll
+            // 
+            this.btRemoveAll.Location = new System.Drawing.Point(77, 46);
+            this.btRemoveAll.Name = "btRemoveAll";
+            this.btRemoveAll.Size = new System.Drawing.Size(71, 50);
+            this.btRemoveAll.TabIndex = 2;
+            this.btRemoveAll.Text = "CE";
+            this.btRemoveAll.UseVisualStyleBackColor = true;
+            // 
+            // btCancle
+            // 
+            this.btCancle.Location = new System.Drawing.Point(154, 46);
+            this.btCancle.Name = "btCancle";
+            this.btCancle.Size = new System.Drawing.Size(71, 50);
+            this.btCancle.TabIndex = 3;
+            this.btCancle.Text = "C";
+            this.btCancle.UseVisualStyleBackColor = true;
+            // 
+            // btBackSpace
+            // 
+            this.btBackSpace.Location = new System.Drawing.Point(231, 46);
+            this.btBackSpace.Name = "btBackSpace";
+            this.btBackSpace.Size = new System.Drawing.Size(71, 50);
+            this.btBackSpace.TabIndex = 4;
+            this.btBackSpace.Text = "<-";
+            this.btBackSpace.UseVisualStyleBackColor = true;
+            // 
+            // btDivision
+            // 
+            this.btDivision.Location = new System.Drawing.Point(231, 102);
+            this.btDivision.Name = "btDivision";
+            this.btDivision.Size = new System.Drawing.Size(71, 50);
+            this.btDivision.TabIndex = 8;
+            this.btDivision.Text = "÷";
+            this.btDivision.UseVisualStyleBackColor = true;
+            // 
+            // btMultiply
+            // 
+            this.btMultiply.Location = new System.Drawing.Point(154, 102);
+            this.btMultiply.Name = "btMultiply";
+            this.btMultiply.Size = new System.Drawing.Size(71, 50);
+            this.btMultiply.TabIndex = 7;
+            this.btMultiply.Text = "*";
+            this.btMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btMinus
+            // 
+            this.btMinus.Location = new System.Drawing.Point(77, 102);
+            this.btMinus.Name = "btMinus";
+            this.btMinus.Size = new System.Drawing.Size(71, 50);
+            this.btMinus.TabIndex = 6;
+            this.btMinus.Text = "-";
+            this.btMinus.UseVisualStyleBackColor = true;
+            // 
+            // btPlus
+            // 
+            this.btPlus.Location = new System.Drawing.Point(0, 102);
+            this.btPlus.Name = "btPlus";
+            this.btPlus.Size = new System.Drawing.Size(71, 50);
+            this.btPlus.TabIndex = 5;
+            this.btPlus.Text = "+";
+            this.btPlus.UseVisualStyleBackColor = true;
+            // 
+            // btFloat
+            // 
+            this.btFloat.Location = new System.Drawing.Point(154, 326);
+            this.btFloat.Name = "btFloat";
+            this.btFloat.Size = new System.Drawing.Size(71, 50);
+            this.btFloat.TabIndex = 12;
+            this.btFloat.Text = ".";
+            this.btFloat.UseVisualStyleBackColor = true;
+            // 
+            // btThree
+            // 
+            this.btThree.Location = new System.Drawing.Point(154, 270);
+            this.btThree.Name = "btThree";
+            this.btThree.Size = new System.Drawing.Size(71, 50);
+            this.btThree.TabIndex = 11;
+            this.btThree.Text = "3";
+            this.btThree.UseVisualStyleBackColor = true;
+            // 
+            // btTwo
+            // 
+            this.btTwo.Location = new System.Drawing.Point(77, 270);
+            this.btTwo.Name = "btTwo";
+            this.btTwo.Size = new System.Drawing.Size(71, 50);
+            this.btTwo.TabIndex = 10;
+            this.btTwo.Text = "2";
+            this.btTwo.UseVisualStyleBackColor = true;
+            // 
+            // btOne
+            // 
+            this.btOne.Location = new System.Drawing.Point(0, 270);
+            this.btOne.Name = "btOne";
+            this.btOne.Size = new System.Drawing.Size(71, 50);
+            this.btOne.TabIndex = 9;
+            this.btOne.Text = "1";
+            this.btOne.UseVisualStyleBackColor = true;
+            // 
+            // btZero
+            // 
+            this.btZero.Location = new System.Drawing.Point(77, 326);
+            this.btZero.Name = "btZero";
+            this.btZero.Size = new System.Drawing.Size(71, 50);
+            this.btZero.TabIndex = 16;
+            this.btZero.Text = "0";
+            this.btZero.UseVisualStyleBackColor = true;
+            // 
+            // btSix
+            // 
+            this.btSix.Location = new System.Drawing.Point(154, 214);
+            this.btSix.Name = "btSix";
+            this.btSix.Size = new System.Drawing.Size(71, 50);
+            this.btSix.TabIndex = 15;
+            this.btSix.Text = "6";
+            this.btSix.UseVisualStyleBackColor = true;
+            // 
+            // btFive
+            // 
+            this.btFive.Location = new System.Drawing.Point(77, 214);
+            this.btFive.Name = "btFive";
+            this.btFive.Size = new System.Drawing.Size(71, 50);
+            this.btFive.TabIndex = 14;
+            this.btFive.Text = "5";
+            this.btFive.UseVisualStyleBackColor = true;
+            // 
+            // btFour
+            // 
+            this.btFour.Location = new System.Drawing.Point(0, 214);
+            this.btFour.Name = "btFour";
+            this.btFour.Size = new System.Drawing.Size(71, 50);
+            this.btFour.TabIndex = 13;
+            this.btFour.Text = "4";
+            this.btFour.UseVisualStyleBackColor = true;
+            // 
+            // btNegative
+            // 
+            this.btNegative.Location = new System.Drawing.Point(0, 326);
+            this.btNegative.Name = "btNegative";
+            this.btNegative.Size = new System.Drawing.Size(71, 50);
+            this.btNegative.TabIndex = 20;
+            this.btNegative.Text = "+/_";
+            this.btNegative.UseVisualStyleBackColor = true;
+            // 
+            // btNine
+            // 
+            this.btNine.Location = new System.Drawing.Point(154, 158);
+            this.btNine.Name = "btNine";
+            this.btNine.Size = new System.Drawing.Size(71, 50);
+            this.btNine.TabIndex = 19;
+            this.btNine.Text = "9";
+            this.btNine.UseVisualStyleBackColor = true;
+            // 
+            // btEight
+            // 
+            this.btEight.Location = new System.Drawing.Point(77, 158);
+            this.btEight.Name = "btEight";
+            this.btEight.Size = new System.Drawing.Size(71, 50);
+            this.btEight.TabIndex = 18;
+            this.btEight.Text = "8";
+            this.btEight.UseVisualStyleBackColor = true;
+            // 
+            // btSeven
+            // 
+            this.btSeven.Location = new System.Drawing.Point(0, 158);
+            this.btSeven.Name = "btSeven";
+            this.btSeven.Size = new System.Drawing.Size(71, 50);
+            this.btSeven.TabIndex = 17;
+            this.btSeven.Text = "7";
+            this.btSeven.UseVisualStyleBackColor = true;
+            // 
+            // btResult
+            // 
+            this.btResult.Location = new System.Drawing.Point(231, 326);
+            this.btResult.Name = "btResult";
+            this.btResult.Size = new System.Drawing.Size(71, 50);
+            this.btResult.TabIndex = 21;
+            this.btResult.Text = "=";
+            this.btResult.UseVisualStyleBackColor = true;
+            // 
+            // pbCuteCat
+            // 
+            this.pbCuteCat.Image = ((System.Drawing.Image)(resources.GetObject("pbCuteCat.Image")));
+            this.pbCuteCat.Location = new System.Drawing.Point(231, 158);
+            this.pbCuteCat.Name = "pbCuteCat";
+            this.pbCuteCat.Size = new System.Drawing.Size(71, 162);
+            this.pbCuteCat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCuteCat.TabIndex = 22;
+            this.pbCuteCat.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 375);
+            this.Controls.Add(this.pbCuteCat);
+            this.Controls.Add(this.btResult);
+            this.Controls.Add(this.btNegative);
+            this.Controls.Add(this.btNine);
+            this.Controls.Add(this.btEight);
+            this.Controls.Add(this.btSeven);
+            this.Controls.Add(this.btZero);
+            this.Controls.Add(this.btSix);
+            this.Controls.Add(this.btFive);
+            this.Controls.Add(this.btFour);
+            this.Controls.Add(this.btFloat);
+            this.Controls.Add(this.btThree);
+            this.Controls.Add(this.btTwo);
+            this.Controls.Add(this.btOne);
+            this.Controls.Add(this.btDivision);
+            this.Controls.Add(this.btMultiply);
+            this.Controls.Add(this.btMinus);
+            this.Controls.Add(this.btPlus);
+            this.Controls.Add(this.btBackSpace);
+            this.Controls.Add(this.btCancle);
+            this.Controls.Add(this.btRemoveAll);
+            this.Controls.Add(this.btPercentage);
+            this.Controls.Add(this.TbResult);
+            this.Name = "MainForm";
+            this.Text = "계산기";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCuteCat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TbResult;
+        private System.Windows.Forms.Button btPercentage;
+        private System.Windows.Forms.Button btRemoveAll;
+        private System.Windows.Forms.Button btCancle;
+        private System.Windows.Forms.Button btBackSpace;
+        private System.Windows.Forms.Button btDivision;
+        private System.Windows.Forms.Button btMultiply;
+        private System.Windows.Forms.Button btMinus;
+        private System.Windows.Forms.Button btPlus;
+        private System.Windows.Forms.Button btFloat;
+        private System.Windows.Forms.Button btThree;
+        private System.Windows.Forms.Button btTwo;
+        private System.Windows.Forms.Button btOne;
+        private System.Windows.Forms.Button btZero;
+        private System.Windows.Forms.Button btSix;
+        private System.Windows.Forms.Button btFive;
+        private System.Windows.Forms.Button btFour;
+        private System.Windows.Forms.Button btNegative;
+        private System.Windows.Forms.Button btNine;
+        private System.Windows.Forms.Button btEight;
+        private System.Windows.Forms.Button btSeven;
+        private System.Windows.Forms.Button btResult;
+        private System.Windows.Forms.PictureBox pbCuteCat;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormCak
+{
+
+    public partial class MainForm : Form
+    {
+        private string formula;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            btOne.Click += BtCommon_Click;
+            btTwo.Click += BtCommon_Click;
+            btThree.Click += BtCommon_Click;
+            btFour.Click += BtCommon_Click;
+            btFive.Click += BtCommon_Click;
+            btSix.Click += BtCommon_Click;
+            btSeven.Click += BtCommon_Click;
+            btEight.Click += BtCommon_Click;
+            btNine.Click += BtCommon_Click;
+            btZero.Click += BtCommon_Click;
+            btPlus.Click += BtCommon_Click;
+            btMinus.Click += BtCommon_Click;
+            btMultiply.Click += BtCommon_Click;
+            btDivision.Click += BtCommon_Click;
+            btPercentage.Click += BtCommon_Click;
+            btFloat.Click += BtCommon_Click;
+
+            btRemoveAll.Click += BtRemoveAll_Click;
+
+            btResult.Click += BtResult_Click;
+        }
+
+        private void BtResult_Click(object sender, EventArgs e)
+        {
+            string result = TbResult.Text;
+
+
+            if (result.Contains("+"))
+            {
+                TbResult.Text = Plus(result.Split('+')[0], result.Split('+')[1]).ToString();
+            }
+            if (result.Contains("-"))
+            {
+                TbResult.Text = Minus(result.Split('-')[0], result.Split('-')[1]).ToString();
+            }
+            if (result.Contains("*"))
+            {
+                TbResult.Text = Mul(result.Split('*')[0], result.Split('*')[1]).ToString();
+            }
+            if (result.Contains("÷"))
+            {
+                TbResult.Text = Div(result.Split('÷')[0], result.Split('÷')[1]).ToString();
+            }
+        }
+        
+        private decimal Plus(string first, string second)
+        {
+            return decimal.Parse(first) + decimal.Parse(second);
+        }
+        private decimal Minus(string first, string second)
+        {
+            return decimal.Parse(first) - decimal.Parse(second);
+        }
+        private decimal Div(string first, string second)
+        {
+            return decimal.Parse(first) / decimal.Parse(second);
+        }
+        private decimal Mul(string first, string second)
+        {
+            return decimal.Parse(first) * decimal.Parse(second);
+        }
+
+
+        private void BtRemoveAll_Click(object sender, EventArgs e)
+        {
+            formula = "";
+            TbResult.Text = formula;
+        }
+
+        private void BtCommon_Click(object sender, EventArgs e)
+        {
+            formula += ((Button)sender).Text;
+            TbResult.Text = formula;
+        }
+    }
+}
